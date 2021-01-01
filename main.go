2f7f50d89659bb4308ab4370f436f1e160093ae6
Haris Razis <dev@xrazis.com>
2021-01-01 09:38:04 +0200

    ➕ Add command to mass delete messages

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -19,52 +21,62 @@ func main() {
 
 	token := os.Getenv("DISCORD_TOKEN")
 
-	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
 	}
 
-	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
 
-	// In this example, we only care about receiving message events.
 	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages)
 
-	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
 
-	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
-	// Cleanly close down the Discord session.
 	dg.Close()
 }
 
-// This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the authenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
+
+	if m.Content == "!ping" {
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
 	}
 
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
+	if m.Content == "!pong" {
 		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
+
+	sp := strings.Split(m.Content, " ")
+
+	if sp[0] == "!delete" {
+		var msgSlice []string
+		var r int
+
+		if len(sp) > 1 {
+			r, _ = strconv.Atoi(sp[1])
+		} else {
+			r = 2
+		}
+
+		msg, _ := s.ChannelMessages(m.ChannelID, r, "", "", "")
+
+		for _, message := range msg {
+			msgSlice = append(msgSlice, message.ID)
+		}
+
+		s.ChannelMessagesBulkDelete(m.ChannelID, msgSlice)
+	}
+
 }
